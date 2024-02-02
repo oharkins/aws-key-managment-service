4a468f3123a55a2fc756935bc8a35353e23be3f7
Odis Harkins <odis.harkins@tylertech.com>
2024-02-01 23:34:37 -0700

    Working!!! 🤪🎊🎉

--- api/functions/public-verify-key/index.js
+++ api/functions/public-verify-key/index.js
@@ -30,13 +30,13 @@ exports.handler = async (event) => {
                 headers: { 'Access-Control-Allow-Origin': process.env.CORS_ORIGIN }
             };
         }
-        const { pk, keyParts } = unmarshall(GetKeyResponse.Items[0]);
+        const keyObject = unmarshall(GetKeyResponse.Items[0]);
 
         const serviceResponse = await ddb.send(new GetItemCommand({
             TableName: process.env.TABLE_NAME,
             Key: marshall({
-                pk: pk,
-                sk: `service#${keyParts.serviceId}`
+                pk: keyObject.pk,
+                sk: `service#${keyObject.keyParts.serviceId}`
             })
         }));
 
@@ -47,7 +47,7 @@ exports.handler = async (event) => {
                 headers: { 'Access-Control-Allow-Origin': process.env.CORS_ORIGIN }
             };
         }
-        const { maxKeyAgeDays , status } = unmarshall(serviceResponse.Item);
+        const { status } = unmarshall(serviceResponse.Item);
 
         if (status === 'Frozen') {
             return {
@@ -56,29 +56,20 @@ exports.handler = async (event) => {
                 headers: {'Access-Control-Allow-Origin': process.env.CORS_ORIGIN}
             };
         }
-        // Attempt to generate a new key 5 times
-        for (let i = 0; i < 5; i++) {
-            const newKey = getKey();
-            const newKeyId = ULID.ulid().toLowerCase();
-            let success = true;
-            try {
-                await saveKey(pk, keyParts.serviceId, newKeyId, newKey, maxKeyAgeDays);
-                await updateKey(pk, keyParts.serviceId, keyParts.keyId);
-            } catch (err) {
-                if (err.name == 'ConditionalCheckFailedException') {
-                    success = false;
-                } else {
-                    throw err;
-                }
-            }
-            if (success) {
-                return {
-                    statusCode: 201,
-                    body: JSON.stringify({ keyId: newKeyId, key: newKey }),
-                    headers: { 'Access-Control-Allow-Origin': process.env.CORS_ORIGIN }
-                };
-            }
-        }
+
+        await updateKey(keyObject.pk, keyObject.keyParts.serviceId, keyObject.keyParts.keyId);
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify(
+                {
+                    serviceID: keyObject.keyParts.serviceId,
+                    status: keyObject.status,
+                    expirationDate: keyObject.expirationDate
+                }),
+            headers: { 'Access-Control-Allow-Origin': process.env.CORS_ORIGIN }
+        };
+
     } catch (err) {
         console.error(err);
         return {
@@ -87,36 +78,6 @@ exports.handler = async (event) => {
             headers: { 'Access-Control-Allow-Origin': process.env.CORS_ORIGIN }
         };
     }
-};
-const getKey = () => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let result = '';
-    const charactersLength = characters.length;
-    for (let i = 0; i < 32; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-};
-
-const saveKey = async (tenantId, serviceId, keyId, key, maxKeyAgeDays) => {
-    let currentDate = new Date();
-    let terminatedDate = new Date(currentDate.setDate(currentDate.getDate() + maxKeyAgeDays));
-
-    await ddb.send(new PutItemCommand({
-        TableName: process.env.TABLE_NAME,
-        ConditionExpression: 'attribute_not_exists(sort)',
-        Item: marshall({
-            pk: tenantId,
-            sk: `key#${serviceId}#${keyId}`,
-            status: 'Active',
-            type: 'key',
-            key: key,
-            sort: key,
-            keyParts: { serviceId: serviceId, keyId: keyId},
-            createdDate: Math.floor(new Date().getTime() / 1000),
-            expirationDate: Math.floor(terminatedDate.getTime() / 1000),
-        })
-    }));
 };
 const updateKey = async (tenantId, serviceId, keyId) => {
     let currentDate = new Date();
@@ -128,14 +89,12 @@ const updateKey = async (tenantId, serviceId, keyId) => {
             pk: {S: tenantId},
             sk: {S: `key#${serviceId}#${keyId}`}
         },
-        UpdateExpression: 'SET #status = :status , #expirationDate = :expirationDate',
+        UpdateExpression: 'SET #lastUsed = :lastUsed',
         ExpressionAttributeNames: {
-            "#status": "status",
-            "#expirationDate": "expirationDate"
+            "#lastUsed": "lastUsed"
         },
         ExpressionAttributeValues: {
-            ':status': { S: 'Expiring' },
-            ':expirationDate': { N: Math.floor(terminatedDate.getTime() / 1000).toString() }
+            ':lastUsed': { N: Math.floor(currentDate.getTime() / 1000).toString() }
         }
     }));
 };
